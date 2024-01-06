49a68a3c9f200c724fb51cef1a7579f81befbbc8
ali sharifi <alisharifyofficial@gmail.com>
2024-01-06 12:48:48 +0330

    fix docs server and add term page

--- GeoIpWeb/views.py
+++ GeoIpWeb/views.py
@@ -9,3 +9,10 @@ from flask import render_template
 def index_get() -> str:
     """Render Index Page"""
     return render_template("web/index.html")
+
+
+
+@web.route("/terms/", methods=["GET"])
+def term_get() -> str:
+    """Render term of use privacy page to user"""
+    return render_template("web/term-of-use.html")
