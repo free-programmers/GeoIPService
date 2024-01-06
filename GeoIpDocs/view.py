32398fe09fc732ccef4e35961d242959e5c30d87
ali sharifi <alisharifyofficial@gmail.com>
2024-01-06 12:49:13 +0330

    fix doc server title

--- GeoIpDocs/view.py
+++ GeoIpDocs/view.py
@@ -7,14 +7,20 @@ from flask import current_app, render_template, abort
 
 
 @docs.route("/<string:documentName>/")
-@ServerCache.cached(timeout=1296000) # 30 day
+@ServerCache.cached(timeout=1296000)  # 30 day
 def serve(documentName: str) -> str:
-    """Serve Docs"""
-    print("action")
+    """
+    Serve Docs
+    this view take a file name and search it in docs folder and if file exists read file and serve it to user
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    this view cache the response
+    """
     documentName += ".html"
+    documentName = documentName.lower()
     if os.path.exists(current_app.config.get("STORAGE_DIR") / "docs" / documentName):
         with open(current_app.config.get("STORAGE_DIR") / "docs" / documentName, mode="r", encoding="utf-8") as f:
             content = f.read()
-            return render_template("docs/document.html", content=content, title=documentName.split(".")[0])
+            documentName = documentName.split(".")[0].capitalize()
+            return render_template("docs/document.html", content=content, title=documentName)
     else:
         abort(404)
