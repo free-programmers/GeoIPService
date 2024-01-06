51cfa00ee8deb3a977782fc167065c6bf105ec78
ali sharifi <alisharifyofficial@gmail.com>
2024-01-06 15:40:50 +0330

    Update template_filter.py

--- GeoIpCore/template_filter.py
+++ GeoIpCore/template_filter.py
@@ -3,16 +3,17 @@ from flask import url_for
 
 
 @app.template_filter('StorageUrl')
-def StorageUrl(path: str):
+def StorageUrl(path: str, external:bool=False):
     """
         this template filter generate dynamic urls base of app.debug mode for serving files via flask or nginx
         if debug mode is set this filter redirect request to nginx
         otherwise return requests to flask serve view
     """
     if app.config.get("DEBUG"):
-        return url_for("FlaskServeStorageFile", path=path)  # flask serve
+        return url_for("FlaskServeStorageFile", path=path, _external=external)  # flask serve
     else:
-        return f"/Storage/{path}"  # Nginx Serve Files
+        return f"{app.config.get('SERVER') if external else ''}/Storage/{path}"  # Nginx Serve Files
+
 
 
 
