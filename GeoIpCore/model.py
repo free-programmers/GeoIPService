fdb1027a5fda7007c46e9e853c508109fd279e99
ali sharifi <alisharifyofficial@gmail.com>
2024-01-12 12:28:02 +0330

    add save method to model

--- GeoIpCore/model.py
+++ GeoIpCore/model.py
@@ -5,6 +5,8 @@ from .extensions import db
 from GeoIpConfig.setting import DATABASE_TABLE_PREFIX_NAME
 from sqlalchemy import String, DateTime, Integer, Column
 
+from flask import current_app
+
 
 class BaseModel(db.Model):
     """
@@ -34,3 +36,17 @@ class BaseModel(db.Model):
     PublicKey = Column(String(36), nullable=False, unique=True)
     CreatedTime = Column(DateTime, default=datetime.datetime.utcnow)
     LastUpdateTime = Column(DateTime, onupdate=datetime.datetime.utcnow, default=datetime.datetime.utcnow)
+
+    def save(self):
+        """
+         combination of two steps, add and commit session
+        """
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            current_app.logger.exception(exc_info=e)
+            return False
+        else:
+            return True
