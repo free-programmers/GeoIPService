ed9f6f3052c166892bc904d4b41393dc39280494
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2024-01-30 16:30:32 +0330

    minor

--- GeoIpCore/context_processor.py
+++ GeoIpCore/context_processor.py
@@ -1,11 +1,13 @@
 from . import app
+
 from flask import session, request
 
 
 @app.context_processor
-def app_context():
+def global_app_context():
+    """This function register template context_processors"""
+    
     ctx = {
         "SERVER_NAME": app.config.get("SERVER", "")
-
     }
     return ctx
