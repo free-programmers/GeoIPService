624d13a41697c71a70e8192fc40689e30cf22fae
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2024-01-30 16:46:41 +0330

    :evergreen_tree: doc string and tpye annotation

--- GeoIpCore/template_filter.py
+++ GeoIpCore/template_filter.py
@@ -4,10 +4,10 @@ from flask import url_for
 
 @app.template_filter('StorageUrl')
 def StorageUrl(path: str, external: bool = False):
-    """
-        this template filter generate dynamic urls base of app.debug mode for serving files via flask or nginx
-        if debug mode is set this filter redirect request to nginx
-        otherwise return requests to flask serve view
+    """ StorageUrl -> dynamic template linking
+        this template filter generates dynamic urls based on app.debug, for determining serving files with flask or Nginx
+        if debug is on this filter redirect request to nginx to be served.
+        otherwise, redirects requests to flask to served <only in debug mode>
     """
     if app.config.get("DEBUG"):
         return url_for("FlaskServeStorageFile", path=path, _external=external)  # flask serve
