569fe8ee2ecbe7b153078e5fefa3af3d8a99b05a
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2024-01-30 16:43:55 +0330

    :fire: fix doc string and type annotations

--- GeoIpCore/model.py
+++ GeoIpCore/model.py
@@ -1,45 +1,52 @@
+# build in
 import uuid
 import datetime
 
-from .extensions import db
-from GeoIpConfig.setting import DATABASE_TABLE_PREFIX_NAME
+# lib
 from sqlalchemy import String, DateTime, Integer, Column
 
+
+# app
+from .extensions import db
+from GeoIpConfig.setting import DATABASE_TABLE_PREFIX_NAME
+
+# framwork
 from flask import current_app
 
 
 class BaseModel(db.Model):
-    """
-    Base model class for all models
-    """
+    """ Base model class for all models in app"""
     __abstract__ = True
 
     id = Column(Integer, primary_key=True)
 
     @staticmethod
-    def SetTableName(name):
-        """Use This Method For setting a table name"""
+    def SetTableName(name:str) -> str:
+        """Use This Method For setting a table name.
+            this method normalize table name and then 
+            added DATABASE TABLE PREFIX NAME to beginning of table name        
+        """
         name = name.replace("-", "_").replace(" ", "")
 
         return f"{DATABASE_TABLE_PREFIX_NAME}{name}"
 
-    def SetPublicKey(self):
-        """This Method Set a Unique PublicKey """
+    def SetPublicKey(self) -> bool:
+        """This Method Set a Unique PublicKey for each record -> uuid"""
         while True:
             token = str(uuid.uuid4())
             if self.query.filter(self.PublicKey == token).first():
                 continue
             else:
                 self.PublicKey = token
-                break
+                return True
+                
 
     PublicKey = Column(String(36), nullable=False, unique=True)
     CreatedTime = Column(DateTime, default=datetime.datetime.utcnow)
     LastUpdateTime = Column(DateTime, onupdate=datetime.datetime.utcnow, default=datetime.datetime.utcnow)
 
-    def save(self):
-        """
-         combination of two steps, add and commit session
+    def save(self) -> bool:
+        """ combination of two steps, add and commit session
         """
         try:
             db.session.add(self)
