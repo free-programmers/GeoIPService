5fa9e2fcedb912443c03d7e04355e8912a15b053
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2024-01-30 16:32:44 +0330

    :camel: add 500 error json response

--- GeoIpCore/errors.py
+++ GeoIpCore/errors.py
@@ -1,10 +1,21 @@
+# Hands errors
+
 from . import app
+
 from flask import jsonify
 
 @app.errorhandler(429)
 def to_many_request(e):
-    return jsonify({"status": "failed", "message":"to many request, 60 request per minute is Allowed"}), 429
+    """ to many request -> view limniter """
+    return jsonify({"status": "failed", "message":"to many request, 60 request per minute is allowed"}), 429
 
 @app.errorhandler(404)
 def not_found_error(e):
-    return jsonify({"status": "failed", "message":"Page not found"}), 404
+    """ page not found error """
+    return jsonify({"status": "failed", "message":"page not found"}), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    """ server error """
+    return jsonify({"status": "failed", "message":"server error "}), 500
