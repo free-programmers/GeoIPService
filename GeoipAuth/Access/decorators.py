c8cfa07e7e0748d65e2f80df5c23a36f277438ad
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2024-02-05 12:54:04 +0330

    :fire: add new decorator manager

--- GeoipAuth/Access/decorators.py
+++ GeoipAuth/Access/decorators.py
@@ -69,3 +69,72 @@ def only_reset_password(f):
         return f(*args, **kwargs)
 
     return inner
+
+
+
+
+
+def login_manager_required(role:int):
+    """ 
+    create custom login_required decorator base on user role
+
+
+    login_required = login_manger_required(role:int=3)
+
+    
+    @app.get("/only/role/3")
+    @login_required
+    def ... ():
+        ...
+    
+    """
+    def login_required(f):
+        """Base Login required Decorator for users"""
+
+        @wraps(f)
+        def inner(*args, **kwargs):
+            next = request.url_rule
+            message = _l("برای دسترسی به صفحه مورد نیاز ابتدا وارد حساب کاربری خود شوید")
+
+            nonlocal role
+            role = role or request.user_role_id or session.get('role') or None
+            if not role:
+                flash(message, "danger")
+                return redirect(url_for("auth.login_get", next=next))
+
+            # check user login
+            if not session.get("is-login", False):
+                flash(message, "danger")
+                return redirect(url_for("auth.login_get", next=next))
+
+            # get user id
+            if not session.get("account-id"):
+                flash(message, "danger")
+                return redirect(url_for("auth.login_get", next=next))
+
+            # check user id
+            try:
+                user = db.session.get(User, session.get("account-id"))
+                if not user:
+                    raise ValueError
+                if user.RoleID != role:
+                    message="access denied"
+                    raise ValueError
+            except Exception as e:
+                flash(message, "danger")
+                return redirect(url_for("auth.login_get", next=next))
+
+
+            # check password
+            if user.Password != (session.get("password")):
+                flash(message, "danger")
+                return redirect(url_for("auth.login_get", next=next))
+
+            if not user.Active:
+                flash(message, "danger")
+                return redirect(url_for("auth.login_get", next=next))
+
+            return f(*args, **kwargs)
+        return inner
+
+    return login_required
