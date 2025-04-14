a5eef83f7be2883f63af01db22a51551f6e27ff3
alisharify <alisharifyofficial@gmail.com>
2025-04-14 14:57:21 +0330

    :fire: add common_ shared library

--- GeoIpConfig/setting.py
+++ GeoIpConfig/setting.py
@@ -1,116 +1,148 @@
-# build in
+"""
+* geoIPservice
+* author: github.com/alisharify7
+* email: alisharifyofficial@gmail.com
+* license: see LICENSE for more details.
+* Copyright (c) 2024 - ali sharifi
+* https://github.com/free-programmers/GeoIPService
+"""
+
 import os
-import datetime
+import pathlib
 from pathlib import Path
 
-# app
-from GeoIpCore.utils import generate_random_string
-
-# libs
 import redis
 from dotenv import load_dotenv
 
+from common_libs.utils import generate_random_string
+
 load_dotenv()
 
-DATABASE_TABLE_PREFIX_NAME = os.environ.get("DATABASE_TABLE_PREFIX_NAME", "")
+
+class BaseSetting:
+    """Base Setting class"""
+
+    SECRET_KEY: str = os.environ.get("APP_SECRET_KEY", generate_random_string())
+    if not os.environ.get("APP_SECRET_KEY", False):
+        print(  # TODO: use logger instead of print
+            "SECRET_KEY was not found in .env file, fall back "
+            "into generate_random_string() function. "
+        )
+    DEBUG: bool = os.environ.get("APP_DEBUG", "") == "True"
+    BASE_DIR: pathlib.Path = Path(__file__).parent.parent.resolve()
+
+    # main API config
+    API_NAME: str = os.environ.get("API_NAME", "api-service")
+    API_REDOC_URL: str = os.environ.get("API_REDOC_URL", "/redoc")
+    API_SWAGGER_URL: str = f'{os.environ.get("API_SWAGGER_URL", "/swagger")}'
+    API_ABSOLUTE_VERSION: str = os.environ.get("API_ABSOLUTE_VERSION", "1.0.0")
+    API_SHORT_VERSION: str = os.environ.get("API_SHORT_VERSION", "1.0.0")
+    API_SUMMERY: str = os.environ.get("API_SUMMERY", "")
+    API_DESCRIPTION: str = os.environ.get("API_DESCRIPTION", "")
+    API_TERM_URL: str = os.environ.get("API_TERM", "/term")
+    API_BASE_URL: str = (
+        f"/{os.environ.get('API_BASE_URL', 'api')}/v{API_SHORT_VERSION}/"
+    )
+
+    # database config
+    DATABASE_NAME: str = os.environ.get("DATABASE_NAME", "")
+    DATABASE_PORT: str = os.environ.get("DATABASE_PORT", "")
+    DATABASE_HOST: str = os.environ.get("DATABASE_HOST", "")
+    DATABASE_USERNAME: str = os.environ.get("DATABASE_USERNAME", "")
+    DATABASE_PASSWORD: str = os.environ.get("DATABASE_PASSWORD", "")
+    DATABASE_TABLE_PREFIX_NAME: str = os.environ.get("DATABASE_TABLE_PREFIX", "")
+    SQLALCHEMY_DATABASE_URI: str = (
+        f"postgresql+asyncpg://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
+    DEBUG_QUERY: bool = os.environ.get("DATABASE_DEBUG_QUERY", "False") == "True"
+
+    def __str__(self):
+        return "BaseSetting Class"
+
+    def __repr__(self):
+        return self.__str__()
 
 
-class Setting:
-    """Flask configuration Class
-    base Setting os.environ class for flask app
+class Setting(BaseSetting):
+    """universal config class
+    every property on this class will be automatically mapping to app.config
     """
 
-    BASE_DIR = Path(__file__).parent.parent
-    APP_DEBUG_STATUS = os.environ.get("APP_DEBUG", "") == "True"
-    SECRET_KEY = os.environ.get("APP_SECRET_KEY", generate_random_string(64))
-    STORAGE_DIR = BASE_DIR.joinpath("Storage")
+    # redis config
+    REDIS_DEFAULT_URI: str = os.environ.get("REDIS_DEFAULT_URI", "localhost")
+    REDIS_DEFAULT_INTERFACE = redis.Redis().from_url(REDIS_DEFAULT_URI)
 
-    # Database Config
-    DATABASE_NAME = os.environ.get("DATABASE_NAME", "")
-    DATABASE_PORT = os.environ.get("DATABASE_PORT", "")
-    DATABASE_HOST = os.environ.get("DATABASE_HOST", "")
-    DATABASE_USERNAME = os.environ.get("DATABASE_USERNAME", "")
-    DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD", "")
-    DATABASE_TABLE_PREFIX_NAME = DATABASE_TABLE_PREFIX_NAME
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_CACHE_URI = os.environ.get("REDIS_CACHE_URI", "localhost")
+    REDIS_CACHE_INTERFACE = redis.Redis().from_url(REDIS_CACHE_URI)
 
-    # Redis Config
-    REDIS_DEFAULT_URI = os.environ.get("REDIS_DEFAULT_URI")
-    REDIS_SERVER = redis.Redis.from_url(REDIS_DEFAULT_URI)
-    # REDIS_PORT = os.environ.get("REDIS_PORT", "")
-    # REDIS_HOST = os.environ.get("REDIS_HOST", "")
-    # REDIS_USERNAME = os.environ.get("REDIS_USERNAME", "")
-    # REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "")
-    # REDIS_DB = os.environ.get("REDIS_DB", 0)
+    REDIS_API_KEY_URI: str = os.environ.get("REDIS_API_KEY_URI", "localhost")
+    REDIS_API_KEY_INTERFACE = redis.Redis.from_url(REDIS_API_KEY_URI)
 
-    # session cookie setting
-    SESSION_TYPE = "redis"
-    SESSION_PERMANENT = False
-    PERMANENT_SESSION_LIFETIME = datetime.timedelta(minutes=24)
-    SESSION_COOKIE_SECURE = True
-    SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SAMESITE = "Lax"
-    SESSION_COOKIE_NAME = "_session_cookie_"
-    SESSION_REDIS = redis.Redis.from_url(
-        os.environ.get("SESSION_REDIS_URI", REDIS_DEFAULT_URI)
+    #  s3 object storage config
+    AWS_BUCKET_NAME: str = os.environ.get("AWS_BUCKET_NAME", "")
+    AWS_ACCESS_KEY_ID: str = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY: str = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_ENDPOINT_URL: str = os.environ.get("AWS_WRITE_ENDPOINT_URL")
+    AWS_READ_ENDPOINT_URL: str = os.environ.get("AWS_READ_ENDPOINT_URL")
+
+    # RabbitMQ Config
+    RABBITMQ_USERNAME: str = os.environ.get("RABBITMQ_USERNAME")
+    RABBITMQ_PASSWORD: str = os.environ.get("RABBITMQ_PASSWORD")
+    RABBITMQ_HOST: str = os.environ.get("RABBITMQ_HOST")
+    RABBITMQ_PORT: int = (
+        5672
+        if not os.environ.get("RABBITMQ_PORT").isnumeric()
+        else int(os.environ.get("RABBITMQ_PORT"))
     )
+    RABBITMQ_VHOST: str = os.environ.get("RABBITMQ_VHOST")
 
-    # Recaptcha Config <Flask-captcha2>
-    RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY", "")
-    RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY", "")
-    RECAPTCHA_ENABLED = os.environ.get("RECAPTCHA_ENABLED", "False") == "True"
-    RECAPTCHA_LOG = os.environ.get("RECAPTCHA_LOG", "True") == "True"
-    # RECAPTCHA_THEME = ''
-    # RECAPTCHA_TYPE = ''
-    # RECAPTCHA_SIZE = ''
-    # RECAPTCHA_LANGUAGE = ''
-    # RECAPTCHA_TABINDEX = ''
+    def __str__(self):
+        return "Setting Class"
 
-    # available languages
-    LANGUAGES = {
-        "fa": "فارسی/Farsi",
-        "en": "English/American English",
-        # 'ar': "عربي/Arabic",
-        # 'tr': "Turkish/Türkçe",
-        # 'ru': "Russian/Россия",
-        # 'zh': "Chinese/中国人",
-    }
+    def __repr__(self):
+        return self.__str__()
 
-    # Mail config
-    MAIL_SERVER = os.getenv("MAIL_SERVER")
-    MAIL_PORT = int(os.getenv("MAIL_PORT"))
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") == "True"
-    MAIL_USE_SSL = False
-    MAIL_DEBUG = os.environ.get("MAIL_DEBUG") == "True"
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
 
-    # Flask-Caching config
-    # https://flask-caching.readthedocs.io/en/latest/  # configuring-flask-caching
-    CACHE_TYPE = (
-        "NullCache" if APP_DEBUG_STATUS else os.environ.get("CACHE_TYPE", "NullCache")
-    )
-    CACHE_DEFAULT_TIMEOUT = (60 * 60) * 12  # ==> 12 hour  # seconds
-    CACHE_REDIS_URL = os.environ.get("CACHE_REDIS_URI", REDIS_DEFAULT_URI)
-    # CACHE_REDIS_HOST = os.environ.get("REDIS_HOST", '')
-    # CACHE_REDIS_PORT = os.environ.get("REDIS_PORT", '')
-    # CACHE_REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", '')
-    # CACHE_REDIS_DB = os.environ.get("CACHE_REDIS_DB", '')
-    # redis: // user: password @ localhost:6379 / 2
+class Production(Setting):
+    """Production config class
 
-    # celery config
-    CELERY = dict(
-        broker_url=os.environ.get("CELERY_BROKER_REDIS_URI", REDIS_DEFAULT_URI),
-        result_backend=os.environ.get("CELERY_BACKEND_REDIS_URI", REDIS_DEFAULT_URI),
-        task_ignore_result=True,
-        broker_connection_retry_on_startup=True,
-        result_serializer="pickle",
-    )
+    use this class for Production config class.
+    """
 
-    DEBUG = APP_DEBUG_STATUS
-    FLASK_DEBUG = APP_DEBUG_STATUS
+    DEBUG: bool = False
 
-    DOMAIN = os.environ.get("SERVER", "")
-    SERVER_NAME = DOMAIN
+    def __str__(self):
+        return "Production Config Class"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Development(Setting):
+    """Development config class
+
+    use this class for Development config class.
+    """
+
+    DEBUG: bool = True
+
+    def __str__(self):
+        return "Development Config Class"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def get_config(debug: bool = BaseSetting.DEBUG) -> object:
+    """Getting config setting class base on `environment` status.
+    :return: object
+    :rtype: object
+    """
+    match debug:
+        case True:
+            return Development
+        case False:
+            return Production
+        case _:
+            return Production
